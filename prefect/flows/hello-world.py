b4699566045ab0e87ca2215778cafdca8d6e72a4
Brock <btibert3@gmail.com>
2025-09-19 16:03:48 +0000

    update for lab4 prefect simple demo

--- prefect/flows/hello-world.py
+++ prefect/flows/hello-world.py
@@ -1,20 +1,27 @@
 from prefect import flow, task
+import random
 
 # Define the first task
-@task
+@task(log_prints=True)
 def task_one():
     print("Executing Task One")
+    fruits = ['Apple', 'Grapes', 'Pear', 'Strawberry']
+    fruit = random.choice(fruits)
+    return fruit
+    
 
 # Define the second task
-@task
-def task_two():
+@task(log_prints=True)
+def task_two(fruit):
     print("Executing Task Two")
+    print("==================")
+    print(f"The randomly selected fruit was: {fruit}")
 
 # Define the flow that calls the tasks
-@flow
-def simple_flow():
-    task_one()
-    task_two()
+@flow(log_prints=True)
+def simple_flow(log):
+    fruit = task_one()
+    task_two(fruit)
 
 # Run the flow
 if __name__ == "__main__":
